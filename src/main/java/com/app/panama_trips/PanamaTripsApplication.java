5d0fb13c11429a2137b4ae37475c8ce6462a3051
David Murillo <david.josue.murillo23@gmail.com>
2025-02-18 10:55:58 -0500

    first commit

---
+++ src/main/java/com/app/panama_trips/PanamaTripsApplication.java
@@ -0,0 +1,13 @@
+package com.app.panama_trips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PanamaTripsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PanamaTripsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/panama_trips/PanamaTripsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.panama_trips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PanamaTripsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
