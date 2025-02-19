f483847103f9b3096ed27226d1b8c1078b352dfb
David Murillo <david.josue.murillo23@gmail.com>
2025-02-19 10:38:11 -0500

    :construction: creating the user entity and its operations

---
+++ src/main/java/com/app/panama_trips/persistence/entity/UserEntity.java
@@ -0,0 +1,59 @@
+package com.app.panama_trips.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "dni", nullable = false, unique = true, length = 13)
+    private String dni;
+
+    @Column(name = "name", nullable = false, length = 75)
+    private String name;
+
+    @Column(name = "lastname", nullable = false, length = 75)
+    private String lastname;
+
+    @Column(name = "email", nullable = false, unique = true, length = 150)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false, columnDefinition = "TEXT")
+    private String passwordHash;
+
+    @Column(name = "profile_image_url", length = 255)
+    private String profileImageUrl;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY) // Relación con la misma entidad (auditoría)
+    @JoinColumn(name = "created_by", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_user_created_by"))
+    private UserEntity createdBy;
+
+    @ManyToOne(fetch = FetchType.LAZY) // Relación con la misma entidad (auditoría)
+    @JoinColumn(name = "updated_by", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_user_updated_by"))
+    private UserEntity updatedBy;
+
+    @Column(name = "deleted_at")
+    private LocalDateTime deletedAt;
+}
