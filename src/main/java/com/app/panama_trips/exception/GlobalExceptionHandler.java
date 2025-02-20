3056a802d15f9dfe8aa529168f3bf68c6cebcf24
David Murillo <david.josue.murillo23@gmail.com>
2025-02-20 10:05:12 -0500

    :technologist: custom response to exceptions

---
+++ src/main/java/com/app/panama_trips/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.app.panama_trips.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.Map;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<Object> handlerUseNotFoundException(UserNotFoundException ex) {
+        Map<String, Object> body = Map.of(
+                "message", ex.getMessage(),
+                "status", HttpStatus.NOT_FOUND.value(),
+                "timestamp", System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(ValidationException.class)
+    public ResponseEntity<Object> handlerValidationException(ValidationException ex) {
+        Map<String, Object> body = Map.of(
+                "message", ex.getMessage(),
+                "status", HttpStatus.BAD_REQUEST.value(),
+                "timestamp", System.currentTimeMillis()
+        );
+
+        return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
+    }
+}
