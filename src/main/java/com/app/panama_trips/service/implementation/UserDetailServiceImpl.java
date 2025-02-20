944849873837f160043b5aaffdba5245bcf23d0d
David Murillo <david.josue.murillo23@gmail.com>
2025-02-20 10:20:15 -0500

    :sparkles:load user details from the database using UserEntityRepository

--- src/main/java/com/app/panama_trips/service/implementation/UserDetailServiceImpl.java
+++ src/main/java/com/app/panama_trips/service/implementation/UserDetailServiceImpl.java
@@ -1,12 +1,38 @@
 package com.app.panama_trips.service.implementation;
 
+import com.app.panama_trips.persistence.entity.UserEntity;
+import com.app.panama_trips.persistence.repository.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class UserDetailServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserEntityRepository userEntityRepository;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return null;
+        UserEntity userEntity = userEntityRepository.findUserEntitiesByName(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+
+        List<SimpleGrantedAuthority> authorityList = new ArrayList<>();
+        authorityList.add(new SimpleGrantedAuthority("ROLE_".concat(userEntity.getRole_id().getName().toString())));
+        userEntity.getRole_id().getPermissions().stream()
+                .forEach(permission -> authorityList.add(new SimpleGrantedAuthority(permission.getName().toString())));
+
+        return new User(
+                userEntity.getName(),
+                userEntity.getPasswordHash(),
+                authorityList
+        );
     }
 }
