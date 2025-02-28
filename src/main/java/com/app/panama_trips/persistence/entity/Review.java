cef1ccf453c3bd7bcf7f9e586b8fa4d005117b27
David Murillo <david.josue.murillo23@gmail.com>
2025-02-28 11:41:07 -0500

    :construction: creation of the Payment and Review entity

---
+++ src/main/java/com/app/panama_trips/persistence/entity/Review.java
@@ -0,0 +1,37 @@
+package com.app.panama_trips.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "reviews")
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(name = "fk_review_user"))
+    private UserEntity userId;
+
+    @ManyToOne
+    @JoinColumn(name = "tour_plan_id", nullable = false, foreignKey = @ForeignKey(name = "fk_review_tour_plan"))
+    private TourPlan tourPlanId;
+
+    @Column(name = "rating", nullable = false)
+    private Integer rating;
+
+    @Column(name = "comment", columnDefinition = "TEXT")
+    private String comment;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+}
