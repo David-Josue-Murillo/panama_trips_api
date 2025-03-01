1eb2eb2d6f800ca2f033b6722f9b3baed27bd835
David Murillo <david.josue.murillo23@gmail.com>
2025-03-01 17:36:16 -0500

    :construction: improve ProvinceService by adding Transactional annotation to improve efficiency when interacting with the DB

--- src/main/java/com/app/panama_trips/service/implementation/ProvinceService.java
+++ src/main/java/com/app/panama_trips/service/implementation/ProvinceService.java
@@ -5,6 +5,8 @@ import com.app.panama_trips.persistence.entity.Province;
 import com.app.panama_trips.persistence.repository.ProvinceRepository;
 import com.app.panama_trips.service.interfaces.IProvinceService;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 @Service
@@ -27,29 +29,34 @@ public class ProvinceService implements IProvinceService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<Province> getAllProvinces() {
         return this.provinceRepository.findAll();
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Province getProvinceById(Integer id) {
         return this.provinceRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Province not found with id " + id));
     }
 
     @Override
+    @Transactional(readOnly = true)
     public Province getProvinceByName(String name) {
         return this.provinceRepository.findByName(name)
                 .orElseThrow(() -> new ResourceNotFoundException("Province not found with name " + name));
     }
 
     @Override
+    @Transactional
     public Province saveProvince(Province province) {
         validateProvince(province);
         return this.provinceRepository.save(province);
     }
 
     @Override
+    @Transactional
     public Province updateProvince(Integer id, Province province) {
         return this.provinceRepository.findById(id)
                 .map(existingProvince -> {
@@ -61,6 +68,7 @@ public class ProvinceService implements IProvinceService {
     }
 
     @Override
+    @Transactional
     public void deleteProvince(Integer id) {
         if(!this.provinceRepository.existsById(id)) {
             throw new ResourceNotFoundException("Province not found with id " + id);
