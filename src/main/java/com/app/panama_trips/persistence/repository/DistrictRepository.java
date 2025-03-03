ee42a86aeaedf04e2afb9ba410a6247e509e5779
David Murillo <david.josue.murillo23@gmail.com>
2025-03-03 15:29:07 -0500

    :card_file_box: add a new query to get a district

--- src/main/java/com/app/panama_trips/persistence/repository/DistrictRepository.java
+++ src/main/java/com/app/panama_trips/persistence/repository/DistrictRepository.java
@@ -4,6 +4,9 @@ import com.app.panama_trips.persistence.entity.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DistrictRepository extends JpaRepository<District, Integer> {
+    Optional<District> findDistrictByName(String name);
 }
