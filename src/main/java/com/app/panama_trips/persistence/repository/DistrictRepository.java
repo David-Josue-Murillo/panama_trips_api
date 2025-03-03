0f8ed38a53c9b0ed2a5e5aaf017fa6b24935edd3
David Murillo <david.josue.murillo23@gmail.com>
2025-03-03 16:28:03 -0500

    :card_file_box: add and modify query

--- src/main/java/com/app/panama_trips/persistence/repository/DistrictRepository.java
+++ src/main/java/com/app/panama_trips/persistence/repository/DistrictRepository.java
@@ -1,12 +1,15 @@
 package com.app.panama_trips.persistence.repository;
 
 import com.app.panama_trips.persistence.entity.District;
+import com.app.panama_trips.persistence.entity.Province;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface DistrictRepository extends JpaRepository<District, Integer> {
-    Optional<District> findDistrictByName(String name);
+    Optional<District> findByName(String name);
+    List<District> findDistrictByProvinceId_Id(Integer provinceIdId);
 }
