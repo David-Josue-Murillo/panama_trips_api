33a9fa42e1fdc941db91bdd7be9cec6694cc88c3
David Murillo <david.josue.murillo23@gmail.com>
2025-03-07 14:57:22 -0500

    :recycle: update ValidationException.java

--- src/main/java/com/app/panama_trips/exception/ValidationException.java
+++ src/main/java/com/app/panama_trips/exception/ValidationException.java
@@ -7,14 +7,7 @@ import java.util.List;
 @ResponseStatus(HttpStatus.BAD_REQUEST)
 public class ValidationException extends RuntimeException {
 
-    private final List<String> errors;
-
-    public ValidationException(String message, List<String> errors) {
+    public ValidationException(String message) {
         super(message);
-        this.errors = errors;
-    }
-
-    public List<String> getErrors() {
-        return errors;
     }
 }
