d665ffecc06e2983d64e989167cb69077af356c4
David Murillo <david.josue.murillo23@gmail.com>
2025-03-08 14:02:12 -0500

    :memo: Add more details to the documentation.

--- src/main/java/com/app/panama_trips/presentation/controller/UserEntityController.java
+++ src/main/java/com/app/panama_trips/presentation/controller/UserEntityController.java
@@ -4,7 +4,12 @@ import com.app.panama_trips.persistence.entity.UserEntity;
 import com.app.panama_trips.presentation.dto.AuthCreateUserRequest;
 import com.app.panama_trips.service.implementation.UserEntityService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
@@ -35,8 +40,13 @@ public class UserEntityController {
             )
     )
     public ResponseEntity<Page<UserEntity>> findAllUsers(
+            @Parameter(description = "Page number (default 0)")
             @RequestParam(defaultValue = "0") Integer page,
+
+            @Parameter(description = "Number of users per page (default: 10)", example = "10")
             @RequestParam(defaultValue = "10") Integer size,
+
+            @Parameter(description = "Enable pagination (default: false)", example = "false")
             @RequestParam(defaultValue = "false") Boolean enabledPagination) {
 
         return ResponseEntity.ok(userEntityService.getAllUser(page, size, enabledPagination));
@@ -64,7 +74,7 @@ public class UserEntityController {
                     )
             )
     )
-    public ResponseEntity<UserEntity> saveUser(@RequestBody AuthCreateUserRequest authCreateUserRequest) {
+    public ResponseEntity<UserEntity> saveUser(@Valid @RequestBody AuthCreateUserRequest authCreateUserRequest) {
         return ResponseEntity.status(HttpStatus.CREATED).body(userEntityService.saveUser(authCreateUserRequest));
     }
 
@@ -73,14 +83,18 @@ public class UserEntityController {
             summary = "Get a user by id",
             description = "Get a user in the system by its id",
             tags = {"User"},
-            responses = @io.swagger.v3.oas.annotations.responses.ApiResponse(
-                    responseCode = "200",
-                    description = "User found",
-                    content = @io.swagger.v3.oas.annotations.media.Content(
-                            mediaType = "application/json",
-                            schema = @io.swagger.v3.oas.annotations.media.Schema(implementation = UserEntity.class)
+            responses = {
+                    @ApiResponse(
+                            responseCode = "200",
+                            description = "User found",
+                            content = @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = UserEntity.class))
+                    ),
+                    @ApiResponse(
+                            responseCode = "404",
+                            description = "User not found"
                     )
-            )
+            }
     )
     public ResponseEntity<UserEntity> findUSerById(@PathVariable Long id) {
         return ResponseEntity.ok(userEntityService.getUserById(id));
