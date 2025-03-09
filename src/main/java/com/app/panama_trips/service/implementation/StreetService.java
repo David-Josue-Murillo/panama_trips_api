a9aa6ecaaaaf2afff482dd57ff6247592fde70ae
David Murillo <david.josue.murillo23@gmail.com>
2025-03-09 16:34:57 -0500

    :fire: Remove input data validations

--- src/main/java/com/app/panama_trips/service/implementation/StreetService.java
+++ src/main/java/com/app/panama_trips/service/implementation/StreetService.java
@@ -8,9 +8,6 @@ import com.app.panama_trips.persistence.repository.StreetRepository;
 import com.app.panama_trips.presentation.dto.StreetRequest;
 import com.app.panama_trips.presentation.dto.StreetResponse;
 import com.app.panama_trips.service.interfaces.IStreetService;
-import jakarta.validation.Valid;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.Pattern;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
@@ -35,7 +32,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional(readOnly = true)
-    public Page<StreetResponse> getAllStreet(@Min(0) Integer page, @Min(1) Integer size, Boolean enabledPagination) {
+    public Page<StreetResponse> getAllStreet(Integer page, Integer size, Boolean enabledPagination) {
         Pageable pageable = enabledPagination
                 ? PageRequest.of(page, size)
                 : Pageable.unpaged();
@@ -45,7 +42,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional(readOnly = true)
-    public List<StreetResponse> getAllStreetByDistrictId(@Min(1) Integer districtId) {
+    public List<StreetResponse> getAllStreetByDistrictId(Integer districtId) {
         return this.streetRepository.findByDistrictId_Id(districtId)
                 .stream()
                 .map(this::convertToResponseDTO)
@@ -54,7 +51,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional(readOnly = true)
-    public StreetResponse getStreetById(@Min(1) Integer id) {
+    public StreetResponse getStreetById(Integer id) {
         return this.streetRepository.findById(id)
                 .map(this::convertToResponseDTO)
                 .orElseThrow(() -> new ResourceNotFoundException("Street with " + id + "not found"));
@@ -62,7 +59,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional(readOnly = true)
-    public StreetResponse getStreetByName(@Pattern(regexp = "^[A-Za-z0-9 ]+$") String name) {
+    public StreetResponse getStreetByName(String name) {
         Street street = this.streetRepository.findStreetByName(name);
         if (street == null) {
             throw new ResourceNotFoundException("Street with name " + name + " not found");
@@ -73,7 +70,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional
-    public StreetResponse saveStreet(@Valid StreetRequest streetRequest) {
+    public StreetResponse saveStreet(StreetRequest streetRequest) {
         this.validateStreetName(streetRequest.name());
         Street street = convertToEntity(streetRequest);
         return convertToResponseDTO(this.streetRepository.save(street));
@@ -81,7 +78,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional
-    public StreetResponse updateStreet(@Min(1) Integer id, @Valid StreetRequest streetRequest) {
+    public StreetResponse updateStreet(Integer id, StreetRequest streetRequest) {
         Street streetExisting = this.streetRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("Street with " + id + "not found"));
         streetExisting.setName(streetRequest.name());
@@ -93,7 +90,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional
-    public void deleteStreet(@Min(1) Integer id) {
+    public void deleteStreet(Integer id) {
         try {
             streetRepository.deleteById(id);
         } catch (EmptyResultDataAccessException e) {
@@ -103,7 +100,7 @@ public class StreetService implements IStreetService {
 
     @Override
     @Transactional
-    public boolean existsStreetByName(@Pattern(regexp = "^[A-Za-z0-9 ]+$") String name) {
+    public boolean existsStreetByName(String name) {
         return this.streetRepository.existsByName(name);
     }
 
