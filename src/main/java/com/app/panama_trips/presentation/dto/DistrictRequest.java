b0f0ff2ef20e41f3fe5e471a6b61091dc1542d9e
David Murillo <david.josue.murillo23@gmail.com>
2025-03-14 16:28:14 -0500

    :art: Add validations

--- src/main/java/com/app/panama_trips/presentation/dto/DistrictRequest.java
+++ src/main/java/com/app/panama_trips/presentation/dto/DistrictRequest.java
@@ -1,4 +1,14 @@
 package com.app.panama_trips.presentation.dto;
 
-public record DistrictRequest(String name, Integer province) {
-}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+public record DistrictRequest(
+        @NotBlank(message = "Name is required")
+        String name,
+
+        @NotNull(message = "Province is required")
+        @Positive(message = "Province must be a positive number")
+        Integer province
+) { }
