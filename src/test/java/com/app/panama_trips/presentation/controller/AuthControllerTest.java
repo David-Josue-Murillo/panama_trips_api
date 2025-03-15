62f1bc42760c8faca825a15b195148beb5f59daf
David Murillo <david.josue.murillo23@gmail.com>
2025-03-14 21:42:16 -0500

    :white_check_mark: Adapting tests to the new changes applied

--- src/test/java/com/app/panama_trips/presentation/controller/AuthControllerTest.java
+++ src/test/java/com/app/panama_trips/presentation/controller/AuthControllerTest.java
@@ -1,95 +1,101 @@
 package com.app.panama_trips.presentation.controller;
 
 import com.app.panama_trips.DataProvider;
+import com.app.panama_trips.exception.ValidationException;
 import com.app.panama_trips.presentation.dto.AuthCreateUserRequest;
 import com.app.panama_trips.presentation.dto.AuthLoginRequest;
 import com.app.panama_trips.presentation.dto.AuthResponse;
 import com.app.panama_trips.service.implementation.UserAuthService;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.web.server.ResponseStatusException;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
-@ExtendWith(MockitoExtension.class)
+import static com.app.panama_trips.DataProvider.*;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(AuthController.class)
 public class AuthControllerTest {
 
-    @Mock
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
     UserAuthService userAuthService;
 
-    @InjectMocks
-    AuthController authController;
-
-    @Test
-    void register_shouldReturnCreatedStatus() {
-        // Given
-        AuthCreateUserRequest authCreateUserRequest = DataProvider.userAuthCreateUserRequestMock();
-        AuthResponse authResponseUser = DataProvider.userAuthResponseMock();
-        when(userAuthService.create(authCreateUserRequest)).thenReturn(authResponseUser);
-
-        // When
-        ResponseEntity<AuthResponse> response = authController.register(authCreateUserRequest);
-
-        // Then
-        assertNotNull(response);
-        assertTrue(response.getStatusCode().is2xxSuccessful());
-        assertEquals(HttpStatus.CREATED, response.getStatusCode());
-        assertEquals(authResponseUser, response.getBody());
+    // Método auxiliar para convertir objetos a JSON
+    private String asJsonString(Object obj) throws Exception {
+        return new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(obj);
     }
 
     @Test
-    void register_shouldReturnConflict_whenUserAlreadyExists() {
-        // Given
-        AuthCreateUserRequest authCreateUserRequest = DataProvider.userAuthCreateUserRequestMock();
-        when(userAuthService.create(authCreateUserRequest)).thenThrow(new ResponseStatusException(HttpStatus.CONFLICT, "User already exists"));
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void register_success() throws Exception {
+        when(userAuthService.create(any(AuthCreateUserRequest.class))).thenReturn(userAuthResponseMock());
 
-        // When
-        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
-            authController.register(authCreateUserRequest);
-        });
-
-        // Then
-        assertEquals(HttpStatus.CONFLICT, exception.getStatusCode());
-        assertEquals("User already exists", exception.getReason());
-    }
-
-
-    @Test
-    void login_shouldReturnOkStatus() {
-        // Given
-        AuthLoginRequest authLoginRequest = DataProvider.userAuthLoginRequestMock();
-        AuthResponse authResponseUser = DataProvider.userAuthResponseMock();
-        when(userAuthService.login(authLoginRequest)).thenReturn(authResponseUser);
-
-        // When
-        ResponseEntity<AuthResponse> response = authController.login(authLoginRequest);
-
-        // Then
-        assertNotNull(response);
-        assertTrue(response.getStatusCode().is2xxSuccessful());
-        assertEquals(HttpStatus.OK, response.getStatusCode());
-        assertEquals(authResponseUser, response.getBody());
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(userAuthCreateUserRequestMock()))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.username").value("admin"))
+                .andExpect(jsonPath("$.jwt").value("jwt"));
     }
 
     @Test
-    void login_shouldReturnUnauthorized_whenCredentialsAreInvalid() {
-        // Given
-        AuthLoginRequest authLoginRequest = DataProvider.userAuthLoginRequestMock();
-        when(userAuthService.login(authLoginRequest)).thenThrow(new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials"));
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void register_badRequest() throws Exception {
+        when(userAuthService.create(any(AuthCreateUserRequest.class))).thenThrow(new BadCredentialsException("Email already exists"));
 
-        // When
-        ResponseStatusException exception = assertThrows(ResponseStatusException.class, () -> {
-            authController.login(authLoginRequest);
-        });
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(userAuthCreateUserRequestMock()))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Email already exists"))
+                .andExpect(jsonPath("$.errorCode").value("BAD_CREDENTIALS"));
+    }
 
-        // Then
-        assertEquals(HttpStatus.UNAUTHORIZED, exception.getStatusCode());
-        assertEquals("Invalid credentials", exception.getReason());
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void login_success() throws Exception {
+        when(userAuthService.login(any(AuthLoginRequest.class))).thenReturn(userAuthResponseMock());
 
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(userAuthLoginRequestMock()))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("admin"))
+                .andExpect(jsonPath("$.jwt").value("jwt"));
+    }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    void login_failed() throws Exception {
+        when(userAuthService.login(any(AuthLoginRequest.class))).thenThrow(new BadCredentialsException("Invalid password"));
+
+        mockMvc.perform(post("/auth/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(asJsonString(userAuthLoginRequestMock()))
+                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.message").value("Invalid password"))
+                .andExpect(jsonPath("$.errorCode").value("BAD_CREDENTIALS"));
     }
 }
