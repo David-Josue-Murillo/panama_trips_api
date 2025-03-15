70395388c3e800454cccaa9aa493c8d480e20a5f
David Murillo <david.josue.murillo23@gmail.com>
2025-03-14 21:40:40 -0500

    :safety_vest: Implementing the annotation that triggers validations

--- src/main/java/com/app/panama_trips/presentation/controller/AuthController.java
+++ src/main/java/com/app/panama_trips/presentation/controller/AuthController.java
@@ -10,6 +10,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
+@Validated
 @Tag(name = "Authentication", description = "Endpoints for authentication")
 public class AuthController {
 
