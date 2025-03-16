198b9a89857e36ea5a0e2de39677f0e9651e4902
David Murillo <david.josue.murillo23@gmail.com>
2025-03-16 16:29:52 -0500

    :recycle: Add repository annotation and changing a parameter

--- src/main/java/com/app/panama_trips/persistence/repository/AddressRepository.java
+++ src/main/java/com/app/panama_trips/persistence/repository/AddressRepository.java
@@ -5,14 +5,16 @@ import com.app.panama_trips.persistence.entity.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface AddressRepository extends JpaRepository<Address, Integer> {
     Optional<Address> findByStreet(String street);
 
-    List<Address> findByDistrictId(District districtId);
+    List<Address> findByDistrictId(Integer district_id);
     List<Address> findAddressesByPostalCode(Integer postalCode);
 
     @Query("SELECT a FROM Address a WHERE LOWER(a.street) LIKE LOWER(concat('%', :streetFragment, '%'))")
