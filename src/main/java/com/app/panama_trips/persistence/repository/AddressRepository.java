fe2a09b9ccc7e1b59f91ee8ac098b84d4c18f3a6
David Murillo <david.josue.murillo23@gmail.com>
2025-03-16 17:16:37 -0500

    :fire: Remove unnecessary code

--- src/main/java/com/app/panama_trips/persistence/repository/AddressRepository.java
+++ src/main/java/com/app/panama_trips/persistence/repository/AddressRepository.java
@@ -1,7 +1,6 @@
 package com.app.panama_trips.persistence.repository;
 
 import com.app.panama_trips.persistence.entity.Address;
-import com.app.panama_trips.persistence.entity.District;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
