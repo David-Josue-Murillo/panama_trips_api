fa7a750cc330a124e05083443e7fd1d62f8536bc
David Murillo <david.josue.murillo23@gmail.com>
2025-03-19 15:56:14 -0500

    :sparkles: Add additional query methods

--- src/main/java/com/app/panama_trips/persistence/repository/ProviderRepository.java
+++ src/main/java/com/app/panama_trips/persistence/repository/ProviderRepository.java
@@ -4,6 +4,24 @@ import com.app.panama_trips.persistence.entity.Provider;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ProviderRepository extends JpaRepository<Provider, Integer> {
+    Optional<Provider> findByRuc(String ruc);
+    Optional<Provider> findByName(String name);
+    Optional<Provider> findByEmail(String email);
+    Optional<Provider> findByPhone(String phone);
+
+    List<Provider> findProvidersByProvince_Id(Integer provinceId);
+    List<Provider> findProvidersByDistrict_Id(Integer districtId);
+    List<Provider> findProvidersByAddress_AddressId(Integer addressAddressId);
+
+    List<Provider> findProvidersByNameContainingIgnoreCase(String name);
+
+    boolean existsProviderByRuc(String ruc);
+    boolean existsProviderByEmail(String email);
+
+    Long countProviders();
 }
