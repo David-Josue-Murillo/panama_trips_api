dbdaf8eb1be6b3b01d923cf6ff5263a936e9061a
David Murillo <david.josue.murillo23@gmail.com>
2025-03-23 16:49:11 -0500

    :poop: Wrong object in parameter was fixed

--- src/main/java/com/app/panama_trips/service/interfaces/IRegionService.java
+++ src/main/java/com/app/panama_trips/service/interfaces/IRegionService.java
@@ -1,5 +1,6 @@
 package com.app.panama_trips.service.interfaces;
 
+import com.app.panama_trips.presentation.dto.RegionRequest;
 import com.app.panama_trips.presentation.dto.RegionResponse;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -8,8 +9,8 @@ public interface IRegionService {
     // Crud operations
     Page<RegionResponse> getAllRegions(Pageable pageable);
     RegionResponse getRegionById(Integer id);
-    RegionResponse saveRegion(RegionResponse regionResponse);
-    RegionResponse updateRegion(Integer id, RegionResponse regionResponse);
+    RegionResponse saveRegion(RegionRequest regionRequest);
+    RegionResponse updateRegion(Integer id, RegionRequest regionRequest);
     void deleteRegion(Integer id);
 
     // Additional service methods
