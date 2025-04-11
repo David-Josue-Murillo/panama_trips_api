26fb01e40efa492004a28f7ba214e66617fa71a2
David Murillo <david.josue.murillo23@gmail.com>
2025-04-10 23:15:45 -0500

    :white_check_mark: Preparing the test environment

--- src/test/java/com/app/panama_trips/presentation/controller/ReservationControllerTest.java
+++ src/test/java/com/app/panama_trips/presentation/controller/ReservationControllerTest.java
@@ -1,7 +1,6 @@
 package com.app.panama_trips.presentation.controller;
 
 import com.app.panama_trips.service.implementation.ReservationService;
-import com.app.panama_trips.service.implementation.TourPlanService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
