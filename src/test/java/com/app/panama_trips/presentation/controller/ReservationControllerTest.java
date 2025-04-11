c1c0771b852d02ccfb2315ac18902ef01e92317f
David Murillo <david.josue.murillo23@gmail.com>
2025-04-10 23:15:38 -0500

    :white_check_mark: Preparing the test environment

---
+++ src/test/java/com/app/panama_trips/presentation/controller/ReservationControllerTest.java
@@ -0,0 +1,23 @@
+package com.app.panama_trips.presentation.controller;
+
+import com.app.panama_trips.service.implementation.ReservationService;
+import com.app.panama_trips.service.implementation.TourPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(ReservationController.class)
+public class ReservationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private ReservationService reservationService;
+
+    // Método auxiliar para convertir objetos a JSON
+    private String asJsonString(Object obj) throws Exception {
+        return new com.fasterxml.jackson.databind.ObjectMapper().writeValueAsString(obj);
+    }
+}
