9e3c5571bdcd6159d586e760434d2f9140c7c873
David Murillo <david.josue.murillo23@gmail.com>
2025-04-10 23:13:48 -0500

    :white_check_mark: Additional methods tests were passed

--- src/test/java/com/app/panama_trips/service/ReservationServiceTest.java
+++ src/test/java/com/app/panama_trips/service/ReservationServiceTest.java
@@ -1,9 +1,9 @@
 package com.app.panama_trips.service;
 
-import com.app.panama_trips.DataProvider;
 import com.app.panama_trips.exception.ResourceNotFoundException;
 import com.app.panama_trips.exception.UserNotFoundException;
 import com.app.panama_trips.persistence.entity.Reservation;
+import com.app.panama_trips.persistence.entity.ReservationStatus;
 import com.app.panama_trips.persistence.repository.ReservationRepository;
 import com.app.panama_trips.persistence.repository.TourPlanRepository;
 import com.app.panama_trips.persistence.repository.UserEntityRepository;
@@ -19,7 +19,8 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import javax.xml.crypto.Data;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.Optional;
 
 import static com.app.panama_trips.DataProvider.*;
@@ -201,5 +202,281 @@ public class ReservationServiceTest {
         // Then
         assertTrue(true);
         verify(reservationRepository, times(1)).deleteById(anyInt());
+    }
+
+    @Test
+    void getReservationByUserId_shouldReturnReservation() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findRecentReservationsByUser(anyLong(), any(LocalDate.class), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationByUserId(1L, pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+        assertEquals(reservationListsMock.getFirst().getId(), result.getContent().getFirst().id());
+    }
+
+    @Test
+    void getReservationByTourPlanId_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByTourPlan_Id(anyInt(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationByTourPlanId(1, pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationByReservationStatus_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByReservationStatus(any(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationByReservationStatus("pending", pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationByReservationDate_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByReservationDate(any(LocalDate.class), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationByReservationDate("2023-10-01", pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsByUserAndStatus_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByUser_IdAndReservationStatus(anyLong(), any(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsByUserAndStatus(1L, "pending", pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsByTourPlanAndStatus_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByTourPlan_IdAndReservationStatus(anyInt(), any(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsByTourPlanAndStatus(1, "pending", pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsBetweenDates_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByReservationDateBetween(any(LocalDate.class), any(LocalDate.class), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsBetweenDates(LocalDate.now(), LocalDate.now().plusDays(10), pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsByMonth_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByReservationDate_Month(anyShort(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsByMonth((short) 10, pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsByYear_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByReservationDate_Year(anyInt(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsByYear(2023, pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsWithPriceGreaterThan_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByTotalPriceGreaterThan(any(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsWithPriceGreaterThan(BigDecimal.valueOf(100), pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsByPriceRange_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByTotalPriceBetween(any(), any(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsByPriceRange(BigDecimal.valueOf(50), BigDecimal.valueOf(150), pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsByDayOfWeek_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByDayOfWeek(anyInt(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsByDayOfWeek(1, pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void getReservationsByProvince_shouldReturnReservations() {
+        // Given
+        Pageable pageable = PageRequest.of(0, 10);
+        PageImpl<Reservation> pageImpl = new PageImpl<>(reservationListsMock, pageable, reservationListsMock.size());
+        when(reservationRepository.findByRegion(anyInt(), any(Pageable.class))).thenReturn(pageImpl);
+
+        // When
+        Page<ReservationResponse> result = reservationService.getReservationsByProvince(1, pageable);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.getContent().size());
+        assertEquals(reservationListsMock.size(), result.getContent().size());
+    }
+
+    @Test
+    void countReservationsByStatus_shouldReturnCount() {
+        // Given
+        when(reservationRepository.countByReservationStatus(any())).thenReturn(5L);
+
+        // When
+        Long result = reservationService.countReservationsByStatus(ReservationStatus.pending);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(5L, result);
+    }
+
+    @Test
+    void countReservationsByTourPlan_shouldReturnCount() {
+        // Given
+        when(reservationRepository.countByTourPlan_Id(anyInt())).thenReturn(10L);
+
+        // When
+        Long result = reservationService.countReservationsByTourPlan(1);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(10L, result);
+    }
+
+    @Test
+    void getReservationStatistics_shouldReturnStatistics() {
+        // Given
+        Object[] statisticsMock = new Object[]{1, 2, 3};
+        when(reservationRepository.getReservationStatistics()).thenReturn(statisticsMock);
+
+        // When
+        Object[] result = reservationService.getReservationStatistics();
+
+        // Then
+        assertNotNull(result);
+        assertEquals(3, result.length);
+        assertArrayEquals(statisticsMock, result);
+    }
+
+    @Test
+    void cancelReservation_shouldCancelReservation() {
+        // Given
+        when(reservationRepository.findById(anyInt())).thenReturn(Optional.of(reservationOneMock));
+
+        // When
+        ReservationResponse result = reservationService.cancelReservation(1);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(reservationOneMock.getId(), result.id());
+    }
+
+    @Test
+    void confirmReservation_shouldConfirmReservation() {
+        // Given
+        when(reservationRepository.findById(anyInt())).thenReturn(Optional.of(reservationOneMock));
+
+        // When
+        ReservationResponse result = reservationService.confirmReservation(1);
+
+        // Then
+        assertNotNull(result);
+        assertEquals(reservationOneMock.getId(), result.id());
     }
 }
