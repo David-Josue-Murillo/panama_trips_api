e6e61651843015ddddd46f851a01e9ad80e4f957
David Murillo <david.josue.murillo23@gmail.com>
2025-04-20 10:30:58 -0500

    :sparkles: add ReviewCategory entity for categorizing review ratings

---
+++ src/main/java/com/app/panama_trips/persistence/entity/ReviewCategory.java
@@ -0,0 +1,24 @@
+package com.app.panama_trips.persistence.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "review_categories")
+public class ReviewCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "description", columnDefinition = "TEXT")
+    private String description;
+}
