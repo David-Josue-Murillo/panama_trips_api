713c390c75371493155511ce107db80ed0eb1e43
David Murillo <david.josue.murillo23@gmail.com>
2025-05-03 22:12:03 -0500

    :sparkles: implement CRUD methods TourPlanImageController

--- src/main/java/com/app/panama_trips/presentation/controller/TourPlanImageController.java
+++ src/main/java/com/app/panama_trips/presentation/controller/TourPlanImageController.java
@@ -1,15 +1,16 @@
 package com.app.panama_trips.presentation.controller;
 
+import com.app.panama_trips.presentation.dto.TourPlanImageRequest;
 import com.app.panama_trips.presentation.dto.TourPlanImageResponse;
 import com.app.panama_trips.service.implementation.TourPlanImageService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tour-plan-image")
@@ -28,5 +29,33 @@ public class TourPlanImageController {
                 ? Pageable.ofSize(size).withPage(page)
                 : Pageable.unpaged();
         return ResponseEntity.ok(this.tourPlanImageService.getAllTourPlanImages(pageable));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TourPlanImageResponse> findTourPlanImageById(@PathVariable Integer id) {
+        return ResponseEntity.ok(this.tourPlanImageService.getTourPlanImageById(id));
+    }
+
+    @PostMapping
+    public ResponseEntity<TourPlanImageResponse> createTourPlanImage(@RequestBody TourPlanImageRequest request) {
+        return new ResponseEntity<>(this.tourPlanImageService.saveTourPlanImage(request), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TourPlanImageResponse> updateTourPlanImage(
+            @PathVariable Integer id,
+            @RequestBody TourPlanImageRequest request) {
+        return ResponseEntity.ok(this.tourPlanImageService.updateTourPlanImage(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteTourPlanImage(@PathVariable Integer id) {
+        this.tourPlanImageService.deleteTourPlanImage(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/tour-plan/{tourPlanId}")
+    public ResponseEntity<TourPlanImageResponse> findImagesByTourPlanId(@PathVariable Integer tourPlanId) {
+        return ResponseEntity.ok(this.tourPlanImageService.getTourPlanImageById(tourPlanId));
     }
 }
