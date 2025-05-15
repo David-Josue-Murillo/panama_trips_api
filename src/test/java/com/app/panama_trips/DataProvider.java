a02b1bda37d0b1a7ad20398e16a1125de3046fd9
David Murillo <david.josue.murillo23@gmail.com>
2025-05-15 15:42:27 -0500

    :sparkles: Add data to test in TourPlanSpecialPrice

--- src/test/java/com/app/panama_trips/DataProvider.java
+++ src/test/java/com/app/panama_trips/DataProvider.java
@@ -849,6 +849,7 @@ public class DataProvider {
     );
 
     public static TourPlanSpecialPriceResponse tourPlanSpecialPriceResponseMock = new TourPlanSpecialPriceResponse(tourPlanSpecialPriceOneMock);
+    public static TourPlanSpecialPriceResponse tourPlanSpecialPriceResponseMockTwo = new TourPlanSpecialPriceResponse(tourPlanSpecialPriceTwoMock);
     public static TourPlanSpecialPriceRequest tourPlanSpecialPriceRequestMock = new TourPlanSpecialPriceRequest(
             1,
             LocalDate.now(),
