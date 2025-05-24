e00d59e7019ec9272e0dedcc10391dc323d8d97b
David Murillo <david.josue.murillo23@gmail.com>
2025-05-23 19:04:15 -0500

    :sparkles: Remove unused import statement from GuideServiceTest to clean up the code

--- src/test/java/com/app/panama_trips/service/GuideServiceTest.java
+++ src/test/java/com/app/panama_trips/service/GuideServiceTest.java
@@ -24,7 +24,6 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
