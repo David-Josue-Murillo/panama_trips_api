46224556125c3549a3fee1c19655a90bd91b8258
David Murillo <david.josue.murillo23@gmail.com>
2025-08-11 17:22:07 -0500

    feat: Initialize PaymentInstallmentServiceTest with mocks and setup method

--- src/test/java/com/app/panama_trips/service/PaymentInstallmentServiceTest.java
+++ src/test/java/com/app/panama_trips/service/PaymentInstallmentServiceTest.java
@@ -1,9 +1,38 @@
 package com.app.panama_trips.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.app.panama_trips.persistence.entity.PaymentInstallment;
+import com.app.panama_trips.persistence.repository.PaymentInstallmentRepository;
+import com.app.panama_trips.presentation.dto.PaymentInstallmentRequest;
+import com.app.panama_trips.service.implementation.PaymentInstallmentService;
+
+import static com.app.panama_trips.DataProvider.*;
 
 @ExtendWith(MockitoExtension.class)
 public class PaymentInstallmentServiceTest {
   
+  @Mock
+  private PaymentInstallmentRepository repository;
+
+  @InjectMocks
+  private PaymentInstallmentService service;
+
+  private PaymentInstallment paymentInstallment;
+  private PaymentInstallmentRequest request;
+  private List<PaymentInstallment> paymentInstallments;
+
+  @BeforeEach
+  void setUp() {
+    paymentInstallment = paymentInstallmentOneMock();
+    request = paymentInstallmentRequestMock;
+    paymentInstallments = paymentInstallmentListMock();
+  }
+
 }
