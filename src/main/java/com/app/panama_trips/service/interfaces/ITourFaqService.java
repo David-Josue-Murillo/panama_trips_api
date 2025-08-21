26d3e79df4fb3c2311560e46c0393bc8b42a9a33
David Murillo <david.josue.murillo23@gmail.com>
2025-08-21 14:31:31 -0500

    feat: Create ITourFaqService interface for FAQ management operations

---
+++ src/main/java/com/app/panama_trips/service/interfaces/ITourFaqService.java
@@ -0,0 +1,15 @@
+package com.app.panama_trips.service.interfaces;
+
+import com.app.panama_trips.presentation.dto.TourFaqRequest;
+import com.app.panama_trips.presentation.dto.TourFaqResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface ITourFaqService {
+    // CRUD operations
+    Page<TourFaqResponse> getAllFaqs(Pageable pageable);
+    TourFaqResponse getFaqById(Integer id);
+    TourFaqResponse saveFaq(TourFaqRequest request);
+    TourFaqResponse updateFaq(Integer id, TourFaqRequest request);
+    void deleteFaq(Integer id);
+}
