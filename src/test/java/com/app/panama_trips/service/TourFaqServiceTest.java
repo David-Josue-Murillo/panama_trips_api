536117d604f9213a59101b050bebfc283bea5283
David Murillo <david.josue.murillo23@gmail.com>
2025-09-02 14:40:24 -0500

    test: Refactor TourFaqServiceTest to initialize mocks in setUp method

--- src/test/java/com/app/panama_trips/service/TourFaqServiceTest.java
+++ src/test/java/com/app/panama_trips/service/TourFaqServiceTest.java
@@ -1,15 +1,23 @@
 package com.app.panama_trips.service;
 
+import static com.app.panama_trips.DataProvider.tourFaqOneMock;
+import static com.app.panama_trips.DataProvider.tourFaqRequestMock;
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
-import org.mockito.exceptions.base.MockitoException;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.app.panama_trips.persistence.entity.TourFaq;
 import com.app.panama_trips.persistence.repository.TourFaqRepository;
 import com.app.panama_trips.persistence.repository.TourPlanRepository;
+import com.app.panama_trips.presentation.dto.TourFaqRequest;
 import com.app.panama_trips.service.implementation.TourFaqService;
+
+import static com.app.panama_trips.DataProvider.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TourFaqServiceTest {
@@ -22,4 +30,15 @@ public class TourFaqServiceTest {
 
     @InjectMocks
     private TourFaqService service;
+
+    private TourFaq tourFaq;
+    private TourFaqRequest tourFaqRequest;
+    private List<TourFaq> tourFaqList;
+
+    @BeforeEach
+    void setUp() {
+        tourFaq = tourFaqOneMock();
+        tourFaqRequest = tourFaqRequestMock;
+        tourFaqList = tourFaqListMock();
+    }
 }
