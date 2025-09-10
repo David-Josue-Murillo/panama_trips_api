c2c7a960e87e3e83e436ef24f035d035cdb9936c
David Murillo <david.josue.murillo23@gmail.com>
2025-09-10 10:23:39 -0500

    feat: Implement CRUD operations in ITourPriceHistoryService interface

--- src/main/java/com/app/panama_trips/service/interfaces/ITourPriceHistoryService.java
+++ src/main/java/com/app/panama_trips/service/interfaces/ITourPriceHistoryService.java
@@ -1,5 +1,16 @@
 package com.app.panama_trips.service.interfaces;
 
+import com.app.panama_trips.presentation.dto.TourPriceHistoryRequest;
+import com.app.panama_trips.presentation.dto.TourPriceHistoryResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 public interface ITourPriceHistoryService {
-    // Aqui va la definicion de los metodos de los servicios
+
+    // CRUD operations
+    Page<TourPriceHistoryResponse> getAllTourPriceHistories(Pageable pageable);
+    TourPriceHistoryResponse getTourPriceHistoryById(Integer id);
+    TourPriceHistoryResponse saveTourPriceHistory(TourPriceHistoryRequest request);
+    TourPriceHistoryResponse updateTourPriceHistory(Integer id, TourPriceHistoryRequest request);
+    void deleteTourPriceHistory(Integer id);
 }
