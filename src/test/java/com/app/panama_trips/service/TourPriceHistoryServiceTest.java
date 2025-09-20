4d6b3a1d2fb76a4832a29cb330433d21ae12337f
David Murillo <david.josue.murillo23@gmail.com>
2025-09-20 15:08:09 -0500

    feat: Enhance TourPriceHistoryServiceTest with setup method and additional mocks

--- src/test/java/com/app/panama_trips/service/TourPriceHistoryServiceTest.java
+++ src/test/java/com/app/panama_trips/service/TourPriceHistoryServiceTest.java
@@ -1,14 +1,19 @@
 package com.app.panama_trips.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.app.panama_trips.persistence.entity.TourPlan;
 import com.app.panama_trips.persistence.entity.TourPriceHistory;
 import com.app.panama_trips.persistence.repository.TourPlanRepository;
 import com.app.panama_trips.persistence.repository.UserEntityRepository;
+import com.app.panama_trips.presentation.dto.TourPriceHistoryRequest;
 import com.app.panama_trips.service.implementation.TourPriceHistoryService;
+
+import static com.app.panama_trips.DataProvider.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TourPriceHistoryServiceTest {
@@ -25,5 +30,15 @@ public class TourPriceHistoryServiceTest {
     @InjectMocks
     private TourPriceHistoryService service;
 
-    
+    private TourPlan tourPlan;
+    private TourPriceHistory priceHistory1, priceHistory2;
+    private TourPriceHistoryRequest request;
+
+    @BeforeEach
+    void setUp() {
+        tourPlan = tourPlanOneMock;
+        priceHistory1 = tourPriceHistoryOneMock();
+        priceHistory2 = tourPriceHistoryTwoMock();
+        request = tourPriceHistoryRequestMock;
+    }
 }
