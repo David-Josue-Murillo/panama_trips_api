caab71629ce9dd7256deda433bfe27abecf45ff0
David Murillo <david.josue.murillo23@gmail.com>
2025-10-14 15:36:04 -0500

    test(audit): enhance AuditLogServiceTest with setup methods and argument captors for improved unit testing

--- src/test/java/com/app/panama_trips/service/AuditLogServiceTest.java
+++ src/test/java/com/app/panama_trips/service/AuditLogServiceTest.java
@@ -1,10 +1,21 @@
 package com.app.panama_trips.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.app.panama_trips.persistence.entity.AuditLog;
+import com.app.panama_trips.persistence.entity.UserEntity;
 import com.app.panama_trips.persistence.repository.AuditLogRepository;
+import com.app.panama_trips.service.implementation.AuditLogService;
+
+import static com.app.panama_trips.DataProvider.*;
 
 @ExtendWith(MockitoExtension.class)
 public class AuditLogServiceTest {
@@ -12,4 +23,26 @@ public class AuditLogServiceTest {
     @Mock
     private AuditLogRepository repository;
 
+    @InjectMocks
+    private AuditLogService service;
+
+    @Captor
+    private ArgumentCaptor<AuditLog> auditLogCaptor;
+
+    @Captor
+    private ArgumentCaptor<List<AuditLog>> auditLogsCaptor;
+
+    @Captor
+    private ArgumentCaptor<List<Integer>> idsCaptor;
+
+    private AuditLog auditLog;
+    private List<AuditLog> auditLogs;
+    private UserEntity user;
+
+    @BeforeEach
+    void setUp() {
+        auditLog = auditLogOneMock();
+        auditLogs = auditLogListMock();
+        user = userAdmin();
+    }
 }
