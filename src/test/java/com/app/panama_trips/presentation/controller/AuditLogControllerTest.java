b7770af8dc36dc56e0710d579583be9a8c040f50
David Murillo <david.josue.murillo23@gmail.com>
2025-10-17 16:53:43 -0500

    test(audit): enhance AuditLogControllerTest with setup methods and JSON conversion utility for improved test coverage

--- src/test/java/com/app/panama_trips/presentation/controller/AuditLogControllerTest.java
+++ src/test/java/com/app/panama_trips/presentation/controller/AuditLogControllerTest.java
@@ -1,11 +1,21 @@
 package com.app.panama_trips.presentation.controller;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import com.app.panama_trips.persistence.entity.AuditLog;
 import com.app.panama_trips.service.implementation.AuditLogService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+import static com.app.panama_trips.DataProvider.*;
+
 
 @WebMvcTest(AuditLogController.class)
 public class AuditLogControllerTest {
@@ -15,4 +25,21 @@ public class AuditLogControllerTest {
 
     @MockitoBean
     private AuditLogService service;
+
+    private AuditLog auditLog;
+    private List<AuditLog> auditLogs;
+
+    @BeforeEach
+    void setUp() {
+        auditLog = auditLogOneMock();
+        auditLogs = auditLogListMock();
+    }
+
+    // Método auxiliar para convertir objetos a JSON
+    private String asJsonString(Object obj) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper.writeValueAsString(obj);
+    }
 }
