83ad1763b25d6d58a72621b2e6a280a30cf5ddba
David Murillo <david.josue.murillo23@gmail.com>
2025-10-27 19:51:31 -0500

    feat(controller): inject ReviewCategoryService into ReviewCategoryController for enhanced functionality

--- src/main/java/com/app/panama_trips/presentation/controller/ReviewCategoryController.java
+++ src/main/java/com/app/panama_trips/presentation/controller/ReviewCategoryController.java
@@ -3,11 +3,16 @@ package com.app.panama_trips.presentation.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.panama_trips.service.implementation.ReviewCategoryService;
+
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/review-categories")
 @RequiredArgsConstructor
 public class ReviewCategoryController {
+  private final ReviewCategoryService service;
+
+  
 
 }
