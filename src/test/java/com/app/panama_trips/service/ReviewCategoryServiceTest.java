181d794c137367eaf0fdaeaaa237375c89abdb79
David Murillo <david.josue.murillo23@gmail.com>
2025-10-28 11:52:29 -0500

    test(service): enhance ReviewCategoryServiceTest with ArgumentCaptor for improved mock verification

--- src/test/java/com/app/panama_trips/service/ReviewCategoryServiceTest.java
+++ src/test/java/com/app/panama_trips/service/ReviewCategoryServiceTest.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.app.panama_trips.persistence.entity.ReviewCategory;
 import com.app.panama_trips.persistence.repository.ReviewCategoryRepository;
+import com.app.panama_trips.presentation.dto.ReviewCategoryRequest;
 import com.app.panama_trips.service.implementation.ReviewCategoryService;
 
 import static com.app.panama_trips.DataProvider.*;
@@ -23,12 +26,18 @@ public class ReviewCategoryServiceTest {
     @InjectMocks
     private ReviewCategoryService service;
 
+    @Captor
+    private ArgumentCaptor<ReviewCategory> categoryCaptor;
+
     private ReviewCategory category;
     private List<ReviewCategory> categoriesList;
+    private ReviewCategoryRequest request;
 
     @BeforeEach
     void setUp() {
         category = reviewCategoryOneMock();
+        category.setId(1);
         categoriesList = reviewCategoryListMock();
+        request = reviewCategoryRequestMock();
     }
 }
