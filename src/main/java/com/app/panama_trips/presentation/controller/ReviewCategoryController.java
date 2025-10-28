c90198bd758d85ed3625315e5202535d3ac06c99
David Murillo <david.josue.murillo23@gmail.com>
2025-10-27 19:50:25 -0500

    feat(controller): add ReviewCategoryController for managing review categories via API endpoints

---
+++ src/main/java/com/app/panama_trips/presentation/controller/ReviewCategoryController.java
@@ -0,0 +1,13 @@
+package com.app.panama_trips.presentation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/review-categories")
+@RequiredArgsConstructor
+public class ReviewCategoryController {
+
+}
