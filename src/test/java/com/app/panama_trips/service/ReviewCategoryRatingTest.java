cb3fdddc66d58e19ef7d5e7a3855bb368191f256
David Murillo <david.josue.murillo23@gmail.com>
2025-11-19 13:03:36 -0500

    feat(test): implement setup method in ReviewCategoryRatingTest to initialize mock data for unit tests

--- src/test/java/com/app/panama_trips/service/ReviewCategoryRatingTest.java
+++ src/test/java/com/app/panama_trips/service/ReviewCategoryRatingTest.java
@@ -1,7 +1,12 @@
 package com.app.panama_trips.service;
 
+import static com.app.panama_trips.DataProvider.reviewCategoryRatingListMock;
+import static com.app.panama_trips.DataProvider.reviewCategoryRatingOneMock;
+import static com.app.panama_trips.DataProvider.reviewCategoryRatingRequestMock;
+
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -37,4 +42,10 @@ public class ReviewCategoryRatingTest {
     private List<ReviewCategoryRating> categoryRatingList;
     private ReviewCategoryRatingRequest request;
 
+    @BeforeEach
+    void setUp() {
+        categoryRating = reviewCategoryRatingOneMock();
+        categoryRatingList = reviewCategoryRatingListMock();
+        request = reviewCategoryRatingRequestMock();
+    }
 }
