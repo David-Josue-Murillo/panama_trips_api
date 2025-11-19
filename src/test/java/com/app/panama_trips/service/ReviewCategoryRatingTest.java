9366c6b2dc35a6a8fa09034815d16725ba65f68c
David Murillo <david.josue.murillo23@gmail.com>
2025-11-19 13:02:02 -0500

    feat(test): enhance ReviewCategoryRatingTest with additional fields and argument captor for improved unit testing

--- src/test/java/com/app/panama_trips/service/ReviewCategoryRatingTest.java
+++ src/test/java/com/app/panama_trips/service/ReviewCategoryRatingTest.java
@@ -1,13 +1,19 @@
 package com.app.panama_trips.service;
 
+import java.util.List;
+
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.app.panama_trips.persistence.entity.ReviewCategoryRating;
 import com.app.panama_trips.persistence.repository.ReviewCategoryRatingRepository;
 import com.app.panama_trips.persistence.repository.ReviewCategoryRepository;
 import com.app.panama_trips.persistence.repository.ReviewRepository;
+import com.app.panama_trips.presentation.dto.ReviewCategoryRatingRequest;
 import com.app.panama_trips.service.implementation.ReviewCategoryRatingService;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,4 +29,12 @@ public class ReviewCategoryRatingTest {
 
     @InjectMocks
     private ReviewCategoryRatingService service;
+
+    @Captor
+    private ArgumentCaptor<ReviewCategoryRating> categoryRatingCaptor;
+
+    private ReviewCategoryRating categoryRating;
+    private List<ReviewCategoryRating> categoryRatingList;
+    private ReviewCategoryRatingRequest request;
+
 }
