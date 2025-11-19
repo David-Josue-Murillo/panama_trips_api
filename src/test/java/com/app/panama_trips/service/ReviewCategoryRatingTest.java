b9ef1a77b77d0286f8dcd155ae2ba59ef898a55b
David Murillo <david.josue.murillo23@gmail.com>
2025-11-19 12:59:04 -0500

    feat(test): inject ReviewCategoryRatingService into ReviewCategoryRatingTest for improved unit testing

--- src/test/java/com/app/panama_trips/service/ReviewCategoryRatingTest.java
+++ src/test/java/com/app/panama_trips/service/ReviewCategoryRatingTest.java
@@ -1,14 +1,19 @@
 package com.app.panama_trips.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.app.panama_trips.persistence.repository.ReviewCategoryRatingRepository;
+import com.app.panama_trips.service.implementation.ReviewCategoryRatingService;
 
 @ExtendWith(MockitoExtension.class)
 public class ReviewCategoryRatingTest {
 
     @Mock
     ReviewCategoryRatingRepository repository;
+
+    @InjectMocks
+    private ReviewCategoryRatingService service;
 }
