a619ec96bdb0aa358a798eac9edf9c9c510362d8
David Murillo <david.josue.murillo23@gmail.com>
2025-12-03 10:31:59 -0500

    refactor(test): reorganize imports and add setup method for ReviewCategoryRatingControllerTest

--- src/test/java/com/app/panama_trips/presentation/controller/ReviewCategoryRatingControllerTest.java
+++ src/test/java/com/app/panama_trips/presentation/controller/ReviewCategoryRatingControllerTest.java
@@ -1,18 +1,24 @@
 package com.app.panama_trips.presentation.controller;
 
-import java.util.List;
-
+import com.app.panama_trips.presentation.dto.ReviewCategoryRatingRequest;
+import com.app.panama_trips.presentation.dto.ReviewCategoryRatingResponse;
+import com.app.panama_trips.service.implementation.ReviewCategoryRatingService;
+import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.app.panama_trips.presentation.dto.ReviewCategoryRatingRequest;
-import com.app.panama_trips.presentation.dto.ReviewCategoryRatingResponse;
-import com.app.panama_trips.service.implementation.ReviewCategoryRatingService;
+import java.util.List;
+
+import static com.app.panama_trips.DataProvider.reviewCategoryRatingRequestMock;
+import static com.app.panama_trips.DataProvider.reviewCategoryRatingResponseListMock;
+import static com.app.panama_trips.DataProvider.reviewCategoryRatingResponseMock;
+
 
 @WebMvcTest(ReviewCategoryRatingController.class)
 public class ReviewCategoryRatingControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -22,4 +28,11 @@ public class ReviewCategoryRatingControllerTest {
     private ReviewCategoryRatingRequest request;
     private ReviewCategoryRatingResponse response;
     private List<ReviewCategoryRatingResponse> responseList;
+
+    @BeforeEach
+    void setUp() {
+        request = reviewCategoryRatingRequestMock();
+        response = reviewCategoryRatingResponseMock();
+        responseList = reviewCategoryRatingResponseListMock();
+    }
 }
