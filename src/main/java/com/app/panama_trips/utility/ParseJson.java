7ede1cf7e142bd60113cfaed0fafd165466e8ad5
David Murillo <david.josue.murillo23@gmail.com>
2025-12-03 10:14:10 -0500

    feat(utility): add ParseJson utility class for converting objects to JSON

---
+++ src/main/java/com/app/panama_trips/utility/ParseJson.java
@@ -0,0 +1,15 @@
+package com.app.panama_trips.utility;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+public class ParseJson {
+    // Método auxiliar para convertir objetos a JSON
+    public static String asJsonString(Object obj) throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return mapper.writeValueAsString(obj);
+    }
+}
