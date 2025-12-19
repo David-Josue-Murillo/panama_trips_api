9d4a759bce2c2ad039460bc9eb4a4982785c8bc4
David Murillo <david.josue.murillo23@gmail.com>
2025-12-18 22:00:41 -0500

    chore(controller): inject LanguageService into LanguageController for improved dependency management

--- src/main/java/com/app/panama_trips/presentation/controller/LanguageController.java
+++ src/main/java/com/app/panama_trips/presentation/controller/LanguageController.java
@@ -3,8 +3,10 @@ package com.app.panama_trips.presentation.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.panama_trips.service.implementation.LanguageService;
+
 @RestController
 @RequestMapping("/api/languages")
 public class LanguageController {
-
+    private final LanguageService service;
 }
