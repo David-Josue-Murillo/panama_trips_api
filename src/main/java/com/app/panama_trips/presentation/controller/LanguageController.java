b7c1d010dedae1c2df7b4924893eb1faec9cd73b
David Murillo <david.josue.murillo23@gmail.com>
2025-12-18 21:59:42 -0500

    feat(controller): add LanguageController for managing language API endpoints

---
+++ src/main/java/com/app/panama_trips/presentation/controller/LanguageController.java
@@ -0,0 +1,10 @@
+package com.app.panama_trips.presentation.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/languages")
+public class LanguageController {
+
+}
