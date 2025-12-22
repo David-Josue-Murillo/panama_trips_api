c19dfb23cf7ec7b675afbe5dfa7de97ce190a9f5
David Murillo <david.josue.murillo23@gmail.com>
2025-12-21 19:53:50 -0500

    test(service): add additional fields to LanguageServiceTest for enhanced unit testing capabilities

--- src/test/java/com/app/panama_trips/service/LanguageServiceTest.java
+++ src/test/java/com/app/panama_trips/service/LanguageServiceTest.java
@@ -1,5 +1,7 @@
 package com.app.panama_trips.service;
 
+import java.util.List;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Captor;
@@ -9,6 +11,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.app.panama_trips.persistence.entity.Language;
 import com.app.panama_trips.persistence.repository.LanguageRepository;
+import com.app.panama_trips.presentation.dto.LanguageRequest;
 import com.app.panama_trips.service.implementation.LanguageService;
 
 @ExtendWith(MockitoExtension.class)
@@ -22,4 +25,9 @@ public class LanguageServiceTest {
 
     @Captor
     private ArgumentCaptor<Language> languageCaptor;
+
+    private Language language;
+    private List<Language> languages;
+    private LanguageRequest languageRequest;
+
 }
