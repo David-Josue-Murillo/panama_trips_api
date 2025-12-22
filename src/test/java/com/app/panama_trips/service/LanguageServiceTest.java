0a79a4026b52fb6d57a653936ab1391d343c687e
David Murillo <david.josue.murillo23@gmail.com>
2025-12-21 19:53:20 -0500

    test(service): enhance LanguageServiceTest with additional mocks and captors for improved unit testing

--- src/test/java/com/app/panama_trips/service/LanguageServiceTest.java
+++ src/test/java/com/app/panama_trips/service/LanguageServiceTest.java
@@ -1,14 +1,25 @@
 package com.app.panama_trips.service;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.app.panama_trips.persistence.entity.Language;
 import com.app.panama_trips.persistence.repository.LanguageRepository;
+import com.app.panama_trips.service.implementation.LanguageService;
 
 @ExtendWith(MockitoExtension.class)
 public class LanguageServiceTest {
     
     @Mock
     private LanguageRepository repository;
+
+    @InjectMocks
+    private LanguageService service;
+
+    @Captor
+    private ArgumentCaptor<Language> languageCaptor;
 }
