754e29cee0f6aec6587a5366c1a1515fe7a9125f
David Murillo <david.josue.murillo23@gmail.com>
2026-01-06 19:51:49 -0500

    test(controller): enhance LanguageControllerTest with MockitoBean for LanguageService and add request/response setup

--- src/test/java/com/app/panama_trips/presentation/controller/LanguageControllerTest.java
+++ src/test/java/com/app/panama_trips/presentation/controller/LanguageControllerTest.java
@@ -1,8 +1,15 @@
 package com.app.panama_trips.presentation.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.app.panama_trips.presentation.dto.LanguageRequest;
+import com.app.panama_trips.presentation.dto.LanguageResponse;
+import com.app.panama_trips.service.implementation.LanguageService;
 
 @WebMvcTest(LanguageController.class)
 public class LanguageControllerTest {
@@ -10,4 +17,10 @@ public class LanguageControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private LanguageService languageService;
+
+    private LanguageRequest request;
+    private LanguageResponse response;
+    private List<LanguageResponse> responseList;
 }
