a869072cfd4b2a220e71889426bd5f9f86a2c9aa
David Murillo <david.josue.murillo23@gmail.com>
2026-01-11 10:54:04 -0500

    feat(service): expand ITourTranslationService with additional methods for managing tour translations

--- src/main/java/com/app/panama_trips/service/interfaces/ITourTranslationService.java
+++ src/main/java/com/app/panama_trips/service/interfaces/ITourTranslationService.java
@@ -15,5 +15,12 @@ public interface ITourTranslationService {
     TourTranslationResponse updateTourTranslation(Integer tourPlanId, String languageCode,TourTranslationRequest request);
     void deleteTourTranslation(Integer tourPlanId, String languageCode);
 
-    
+    // Additional service methods
+    List<TourTranslationResponse> getTourTranslationsByTourPlanId(Integer tourPlanId);
+    List<TourTranslationResponse> getTourTranslationsByLanguageCode(String languageCode);
+    boolean existsByTourPlanIdAndLanguageCode(Integer tourPlanId, String languageCode);
+    void deleteAllTranslationsByTourPlanId(Integer tourPlanId);
+    void deleteAllTranslationsByLanguageCode(String languageCode);
+    Long countTranslationsByTourPlanId(Integer tourPlanId);
+    long countAllTourTranslations();
 }
