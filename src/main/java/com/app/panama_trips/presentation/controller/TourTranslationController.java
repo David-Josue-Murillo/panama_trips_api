7f2e4284e9915e9e627dc50e4d11f94cd334e99a
David Murillo <david.josue.murillo23@gmail.com>
2026-01-13 15:53:03 -0500

    feat(controller): add getAllTourTranslations endpoint to TourTranslationController for paginated retrieval of tour translations

--- src/main/java/com/app/panama_trips/presentation/controller/TourTranslationController.java
+++ src/main/java/com/app/panama_trips/presentation/controller/TourTranslationController.java
@@ -1,8 +1,13 @@
 package com.app.panama_trips.presentation.controller;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.app.panama_trips.presentation.dto.TourTranslationResponse;
 import com.app.panama_trips.service.implementation.TourTranslationService;
 
 import lombok.RequiredArgsConstructor;
@@ -13,4 +18,9 @@ import lombok.RequiredArgsConstructor;
 public class TourTranslationController {
     private final TourTranslationService service;
 
+    // CRUD operations
+    @GetMapping
+    public ResponseEntity<Page<TourTranslationResponse>> getAllTourTranslations(Pageable pageable) {
+        return ResponseEntity.ok(service.getAllTourTranslations(pageable));
+    }
 }
