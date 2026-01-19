86ca9f4dbf7373574c7c47730a8bde13171f8198
David Murillo <david.josue.murillo23@gmail.com>
2026-01-19 15:17:39 -0500

    test(service): enhance TourTranslationServiceTest with additional setup and mock data for improved unit testing

--- src/test/java/com/app/panama_trips/service/TourTranslationServiceTest.java
+++ src/test/java/com/app/panama_trips/service/TourTranslationServiceTest.java
@@ -1,14 +1,25 @@
 package com.app.panama_trips.service;
 
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.app.panama_trips.persistence.entity.Language;
+import com.app.panama_trips.persistence.entity.TourPlan;
+import com.app.panama_trips.persistence.entity.TourTranslation;
 import com.app.panama_trips.persistence.repository.LanguageRepository;
 import com.app.panama_trips.persistence.repository.TourPlanRepository;
 import com.app.panama_trips.persistence.repository.TourTranslationRepository;
+import com.app.panama_trips.presentation.dto.TourTranslationRequest;
 import com.app.panama_trips.service.implementation.TourTranslationService;
+
+import static com.app.panama_trips.DataProvider.*;
 
 @ExtendWith(MockitoExtension.class)
 public class TourTranslationServiceTest {
@@ -23,4 +34,22 @@ public class TourTranslationServiceTest {
 
     @InjectMocks
     private TourTranslationService service;
+
+    @Captor
+    private ArgumentCaptor<TourTranslation> tourTranslationCaptor;
+
+    private TourTranslation tourTranslation;
+    private List<TourTranslation> tourTranslations;
+    private TourTranslationRequest tourTranslationRequest;
+    private TourPlan tourPlan;
+    private Language language;
+
+    @BeforeEach
+    void setUp() {
+        tourTranslation = tourTranslationSpanishMock();
+        tourTranslations = tourTranslationListMock();
+        tourTranslationRequest = tourTranslationRequestMock();
+        tourPlan = tourPlanOneMock;
+        language = languageSpanishMock();
+    }
 }
