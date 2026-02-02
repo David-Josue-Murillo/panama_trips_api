42df4c74c3083f5f4b88d995c110ec35d0674d47
David Murillo <david.josue.murillo23@gmail.com>
2026-02-02 08:25:17 -0500

    refactor(security): enhance SecurityConfig to use BCryptPasswordEncoder and streamline session management for improved security

--- src/main/java/com/app/panama_trips/security/configuration/SecurityConfig.java
+++ src/main/java/com/app/panama_trips/security/configuration/SecurityConfig.java
@@ -10,18 +10,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.session.SessionRegistryImpl;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
@@ -34,10 +30,10 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         return httpSecurity
-                .httpBasic(Customizer.withDefaults())
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests( auth -> {
+                .authorizeHttpRequests(auth -> {
                     auth.requestMatchers("/auth/**").permitAll();
+                    auth.requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll();
                     auth.requestMatchers("/api/user").hasRole(RoleEnum.ADMIN.name());
                     auth.requestMatchers("/api/user/**").hasRole(RoleEnum.ADMIN.name());
                     auth.requestMatchers("/api/districts").hasRole(RoleEnum.ADMIN.name());
@@ -46,22 +42,7 @@ public class SecurityConfig {
                     auth.requestMatchers("/api/provinces/**").hasRole(RoleEnum.ADMIN.name());
                     auth.anyRequest().authenticated();
                 })
-                .formLogin( formLogin -> {
-                    formLogin.permitAll();
-                    formLogin.successHandler(this.successHandler());
-                })
-                .sessionManagement( session -> {
-                    session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS);
-                    session.invalidSessionUrl("/login");
-                    session.maximumSessions(1).sessionRegistry(this.sessionRegistry());
-                    session.sessionFixation().migrateSession();
-                })
-                .logout( logout -> {
-                    logout.logoutUrl("/logout");
-                    logout.logoutSuccessUrl("/login");
-                    logout.invalidateHttpSession(true);
-                    logout.deleteCookies("JSESSIONID");
-                })
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .addFilterBefore(new JwtTokenValidator(this.jwtUtil), BasicAuthenticationFilter.class)
                 .build();
     }
@@ -80,18 +61,7 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SessionRegistry sessionRegistry() {
-        return new SessionRegistryImpl();
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
-
-    private AuthenticationSuccessHandler successHandler() {
-        return ((request, response, authentication) -> {
-            response.sendRedirect("/api/user");
-        });
+        return new BCryptPasswordEncoder();
     }
 }
