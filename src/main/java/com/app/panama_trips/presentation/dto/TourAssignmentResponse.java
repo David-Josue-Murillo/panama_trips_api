7bb692d04cda774a0fc15582793513f903de430e
David Murillo <david.josue.murillo23@gmail.com>
2026-02-28 22:50:58 -0500

    refactor: Clean up and refactor test data for permissions, roles, and users in `DataProvider.java` and adjust related DTOs and services.

--- src/main/java/com/app/panama_trips/presentation/dto/TourAssignmentResponse.java
+++ src/main/java/com/app/panama_trips/presentation/dto/TourAssignmentResponse.java
@@ -6,27 +6,26 @@ import java.time.LocalDate;
 import com.app.panama_trips.persistence.entity.TourAssignment;
 
 public record TourAssignmentResponse(
-    Integer id,
-    String guideName,
-    String tourPlanName,
-    BigDecimal tourPlanPrice,
-    String duration,
-    LocalDate reservationDate,
-    String status,
-    String notes,
-    LocalDate createdAt
-) {
+        Integer id,
+        String guideName,
+        String tourPlanName,
+        BigDecimal tourPlanPrice,
+        String duration,
+        LocalDate reservationDate,
+        String status,
+        String notes,
+        LocalDate createdAt) {
     public TourAssignmentResponse(TourAssignment tourAssignment) {
         this(
-            tourAssignment.getId(),
-            tourAssignment.getGuide().getUser().getName() + " " + tourAssignment.getGuide().getUser().getLastname(),
-            tourAssignment.getTourPlan().getTitle(),
-            tourAssignment.getTourPlan().getPrice(),
-            tourAssignment.getTourPlan().getDuration() + " days",
-            tourAssignment.getReservationDate(),
-            tourAssignment.getStatus(),
-            tourAssignment.getNotes(),
-            tourAssignment.getCreatedAt()
-        );
+                tourAssignment.getId(),
+                tourAssignment.getGuide().getUser().getName() + " " + tourAssignment.getGuide().getUser().getLastname(),
+                tourAssignment.getTourPlan().getTitle(),
+                tourAssignment.getTourPlan().getPricing() != null ? tourAssignment.getTourPlan().getPricing().getPrice()
+                        : null,
+                tourAssignment.getTourPlan().getDuration() + " days",
+                tourAssignment.getReservationDate(),
+                tourAssignment.getStatus(),
+                tourAssignment.getNotes(),
+                tourAssignment.getCreatedAt());
     }
 }
