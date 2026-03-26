000e21b8f9fcee2fb820b85a7e509cc3fc8c9312
David Murillo <david.josue.murillo23@gmail.com>
2026-03-26 11:32:23 -0500

    test: add ComarcaControllerTest class.

---
+++ src/test/java/com/app/panama_trips/presentation/controller/ComarcaControllerTest.java
@@ -0,0 +1,38 @@
+package com.app.panama_trips.presentation.controller;
+
+import com.app.panama_trips.DataProvider;
+import com.app.panama_trips.exception.ResourceNotFoundException;
+import com.app.panama_trips.presentation.dto.ComarcaRequest;
+import com.app.panama_trips.service.interfaces.IComarcaService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(ComarcaController.class)
+class ComarcaControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Autowired
+  private ObjectMapper objectMapper;
+
+  @MockitoBean
+  private IComarcaService comarcaService;
+
+ 
+}
